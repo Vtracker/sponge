ea77566a62efeb290b000255096dedbc9116fb75
azusa <704580729@qq.com>
2022-09-10 21:26:59 +0800

    lab3 done

--- libsponge/tcp_sender.hh
+++ libsponge/tcp_sender.hh
@@ -8,6 +8,7 @@
 
 #include <functional>
 #include <queue>
+#include <utility>
 
 //! \brief The "sender" part of a TCP implementation.
 
@@ -17,6 +18,18 @@
 //! segments if the retransmission timer expires.
 class TCPSender {
   private:
+    int _timeout{-1};
+    int _timecount{0};
+
+    size_t _outgoing_bytes{0};
+
+    size_t _last_window_size{1};
+    bool _set_syn_flag{false};
+    bool _set_fin_flag{false};
+    size_t _consecutive_retransmissions_count{0};
+
+    std::queue<std::pair<size_t, TCPSegment>> _flyings_segs{};
+
     //! our initial sequence number, the number for our SYN.
     WrappingInt32 _isn;
 
